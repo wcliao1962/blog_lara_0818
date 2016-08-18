01957ae389e6392fa9d551b218a3cc4fef6c2510
wen-chung liao <wcliao1962@gmail.com>
2016-08-18 11:44:28 +0800

    install tracy package

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Recca0120\LaravelTracy\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
